7c9e229c3ed74e26390858d8bb564ed2b90a010f
Spacedwog <owner@spacedwog.com.br>
2025-07-04 19:50:09 -0300

    Commited

--- backend-ts/server.ts
+++
@@ -1,8 +0,0 @@
-// runPython.ts
-import { exec } from 'child_process';
-
-exec('python server.py', (error, stdout, stderr) => {
-  if (error) return console.error(`Erro: ${error.message}`);
-  if (stderr) return console.error(`stderr: ${stderr}`);
-  console.log(`stdout: ${stdout}`);
-});
